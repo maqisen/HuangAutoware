ce600b81aacb540c7d0edc5f85f0dcd06f781230
Sam Bardsley <bardsls@wwu.edu>
2025-04-16 15:48:09 -0700

    Added orientation condition to near_takeover_record

--- ExecuteAlgorithm.cpp
+++ ExecuteAlgorithm.cpp
@@ -12,17 +12,22 @@
 // requires:
 // #include <autoware/motion_utils/trajectory/conversion.hpp>
 // #include <autoware/motion_utils/trajectory/trajectory.hpp>
+// #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 //
 // run with obj.kinematics.initial_pose_with_covariance.pose
 // Pose near_takeover_record (const Pose & current_pose) {
-//   const tolerance = 1;
+//   const distTolerance = 1;
+//   const yawTolerance = 0.2;
 //   const double cur_x = current_pose.position.x;
 //   const double cur_y = current_pose.position.y;
+//   const double cur_yaw = tf2::getYaw(current_pose.orientation);
 //   for (Pose prev_pose : storedPoses) {
 //     const double prev_x = prev_pose.position.x;
 //     const double prev_y = prev_pose.position.y;
+//     const double pev_yaw = tf2::getYaw(prev_pose.orientation);
 //     const Pose dist = hypot((prev_x-cur_x), (prev_y-cur_y));
-//     if (dist < tolerance) return prev_pose; TODO: ensure that direction also matches (pose.orientation)
+//     const double yaw_dif = abs(cur_yaw-prev_yaw);
+//     if (dist < distTolerance && yaw_dif < yawTolerance) return prev_pose;
 //   }
 //  return null;
 // }
